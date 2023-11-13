cf9e2e0e70f95e2357f651f7f8e7c1ddb92c17ac
Queengirl1990 <bruehlanja90@gmail.com>
2023-11-13 20:46:32 +0100

    loadingpage

--- lib/datenspeicher/styles.dart
+++ lib/datenspeicher/styles.dart
@@ -2,3 +2,5 @@ import 'package:flutter/material.dart';
 
 const Color lexiBrown = Color(0xFF837471);
 const Color backgroundGrey = Color(0xFFF3F3F3);
+const Color lexiBlack = Color(0xFF343131);
+
--- lib/screens/loadingPage.dart
+++ lib/screens/loadingPage.dart
@@ -5,7 +5,6 @@ void main() {
   runApp(MyApp());
 }
 
-
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -18,4 +17,73 @@ class MyApp extends StatelessWidget {
 class LoadingPage extends StatefulWidget {
   @override
   _LoadingPageState createState() => _LoadingPageState();
+}
+
+class _LoadingPageState extends State<LoadingPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/lexi.jpg'),
+            fit: BoxFit.fill,
+          ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(height: 50),
+            Padding(
+              padding: EdgeInsets.only(left: 20),
+              child: Text(
+                "Lexis Best",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 28,
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 20, top: 10),
+              child: Text(
+                "Nur das beste für unsere Lieblinge",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 14,
+                ),
+              ),
+            ),
+            Expanded(
+              child: Align(
+                alignment: Alignment.bottomCenter,
+                child: Padding(
+                  padding: const EdgeInsets.only(bottom: 40),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      // Aktion einfügen
+                    },
+                    style: ElevatedButton.styleFrom(
+                      primary: lexiBlack,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                      padding: EdgeInsets.symmetric(vertical: 15, horizontal: 40),
+                    ),
+                    child: Text(
+                      "Start",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
