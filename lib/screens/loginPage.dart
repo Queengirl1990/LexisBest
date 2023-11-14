44573809d25c627a8d6b51903391de123064e6bb
Queengirl1990 <bruehlanja90@gmail.com>
2023-11-14 22:09:57 +0100

    update login

--- lib/screens/loginPage.dart
+++ lib/screens/loginPage.dart
@@ -10,160 +10,190 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: SafeArea(
-          child: Center(
-            child: SingleChildScrollView(
-              child: Padding(
-                padding: EdgeInsets.symmetric(horizontal: 20.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      'Willkommen',
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 24.0,
-                      ),
-                    ),
-                    SizedBox(height: 20.0),
-                    TextField(
-                      decoration: InputDecoration(
-                        hintText: 'Username',
-                        filled: true,
-                        fillColor: lexiBrown,
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(20.0),
+        body: Column(
+          children: [
+            Container(
+              height: 20.0, // Hier die gewünschte Höhe für den oberen Abstand einstellen
+            ),
+            Expanded(
+              child: SingleChildScrollView(
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 20.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(height: 40.0),
+                      Text(
+                        'Willkommen',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 24.0,
                         ),
-                        hintStyle: TextStyle(color: Colors.white),
                       ),
-                      style: TextStyle(color: Colors.white),
-                    ),
-                    SizedBox(height: 10.0),
-                    TextField(
-                      obscureText: true,
-                      decoration: InputDecoration(
-                        hintText: 'Passwort',
-                        filled: true,
-                        fillColor: lexiBrown,
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(20.0),
+                      SizedBox(height: 20.0),
+                      TextField(
+                        decoration: InputDecoration(
+                          hintText: 'Username',
+                          filled: true,
+                          fillColor: lexiBrown,
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20.0),
+                          ),
+                          hintStyle: TextStyle(color: Colors.white),
                         ),
-                        hintStyle: TextStyle(color: Colors.white),
+                        style: TextStyle(color: Colors.white, fontSize: 12.0),
                       ),
-                      style: TextStyle(color: Colors.white),
-                    ),
-                    SizedBox(height: 10.0),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        GestureDetector(
-                          onTap: () {
-                            // Popup-Navigation
+                      SizedBox(height: 10.0),
+                      TextField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          hintText: 'Passwort',
+                          filled: true,
+                          fillColor: lexiBrown,
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20.0),
+                          ),
+                          hintStyle: TextStyle(color: Colors.white),
+                        ),
+                        style: TextStyle(color: Colors.white, fontSize: 12.0),
+                      ),
+                      SizedBox(height: 10.0),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          GestureDetector(
+                            onTap: () {
+                              // Hier Popup-Navigation für "Passwort vergessen" implementieren
+                            },
+                            child: Text(
+                              'Passwort vergessen',
+                              style: TextStyle(color: lexiYellow),
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 20.0),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Icon(Icons.mail, size: 40.0),
+                          ),
+                          SizedBox(width: 100.0),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Icon(Icons.apple, size: 40.0),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 20.0),
+                      Center(
+                        child: ElevatedButton(
+                          onPressed: () {
+                            // Hier den Login-Prozess implementieren
                           },
+                          style: ElevatedButton.styleFrom(
+                            primary: Colors.black,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20.0),
+                            ),
+                          ),
                           child: Text(
-                            'Passwort vergessen',
-                            style: TextStyle(color: lexiYellow),
+                            'Login',
+                            style: TextStyle(color: Colors.white),
                           ),
                         ),
-                      ],
-                    ),
-                    SizedBox(height: 20.0),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Icon(Icons.mail, size: 30.0),
-                        ),
-                        SizedBox(width: 100.0),
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Icon(Icons.apple, size: 30.0),
-                        ),
-                      ],
-                    ),
-                    SizedBox(height: 20.0),
-                    Center(
-                      child: ElevatedButton(
-                        onPressed: () {
-                          // Login-Prozess implementieren
-                        },
-                        style: ElevatedButton.styleFrom(
-                          primary: Colors.black,
-                          shape: RoundedRectangleBorder(
+                      ),
+                      SizedBox(height: 20.0),
+                      TextField(
+                        decoration: InputDecoration(
+                          hintText: 'Username',
+                          filled: true,
+                          fillColor: lexiBrown,
+                          border: OutlineInputBorder(
                             borderRadius: BorderRadius.circular(20.0),
                           ),
+                          hintStyle: TextStyle(color: Colors.white),
                         ),
-                        child: Text(
-                          'Login',
-                          style: TextStyle(color: Colors.white),
-                        ),
+                        style: TextStyle(color: Colors.white, fontSize: 12.0),
                       ),
-                    ),
-                    SizedBox(height: 20.0),
-                    TextField(
-                      decoration: InputDecoration(
-                        hintText: 'Username',
-                        filled: true,
-                        fillColor: lexiBrown,
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(20.0),
-                        ),
-                        hintStyle: TextStyle(color: Colors.white),
-                      ),
-                      style: TextStyle(color: Colors.white),
-                    ),
-                    SizedBox(height: 10.0),
-                    TextField(
-                      obscureText: true,
-                      decoration: InputDecoration(
-                        hintText: 'Passwort',
-                        filled: true,
-                        fillColor: lexiBrown,
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(20.0),
-                        ),
-                        hintStyle: TextStyle(color: Colors.white),
-                      ),
-                      style: TextStyle(color: Colors.white),
-                    ),
-                    SizedBox(height: 10.0),
-                    TextField(
-                      decoration: InputDecoration(
-                        hintText: 'Email',
-                        filled: true,
-                        fillColor: lexiBrown,
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(20.0),
-                        ),
-                        hintStyle: TextStyle(color: Colors.white),
-                      ),
-                      style: TextStyle(color: Colors.white),
-                    ),
-                    SizedBox(height: 20.0),
-                    Center(
-                      child: ElevatedButton(
-                        onPressed: () {
-                          // Hier den Registrierungsprozess implementieren
-                        },
-                        style: ElevatedButton.styleFrom(
-                          primary: Colors.black,
-                          shape: RoundedRectangleBorder(
+                      SizedBox(height: 10.0),
+                      TextField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          hintText: 'Passwort',
+                          filled: true,
+                          fillColor: lexiBrown,
+                          border: OutlineInputBorder(
                             borderRadius: BorderRadius.circular(20.0),
                           ),
+                          hintStyle: TextStyle(color: Colors.white),
                         ),
-                        child: Text(
-                          'Registrieren',
-                          style: TextStyle(color: Colors.white),
+                        style: TextStyle(color: Colors.white, fontSize: 12.0),
+                      ),
+                      SizedBox(height: 10.0),
+                      TextField(
+                        decoration: InputDecoration(
+                          hintText: 'Email',
+                          filled: true,
+                          fillColor: lexiBrown,
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20.0),
+                          ),
+                          hintStyle: TextStyle(color: Colors.white),
+                        ),
+                        style: TextStyle(color: Colors.white, fontSize: 12.0),
+                      ),
+                      SizedBox(height: 20.0),
+                      Center(
+                        child: ElevatedButton(
+                          onPressed: () {
+                            // Registrierungsprozess 
+                          },
+                          style: ElevatedButton.styleFrom(
+                            primary: Colors.black,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20.0),
+                            ),
+                          ),
+                          child: Text(
+                            'Registrieren',
+                            style: TextStyle(color: Colors.white),
+                          ),
                         ),
                       ),
-                    ),
-                  ],
+                      SizedBox(height: 10.0),
+                      CheckboxListTile(
+                        title: Text(
+                          'Ich stimme den AGBs zu',
+                          style: TextStyle(color: Colors.black, fontSize: 12.0),
+                        ),
+                        value: false, 
+                        onChanged: (bool? value) {
+                          // Aktion der Checkbox 
+                        },
+                        controlAffinity: ListTileControlAffinity.leading,
+                        contentPadding: EdgeInsets.symmetric(horizontal: 0.0),
+                      ),
+                      CheckboxListTile(
+                        title: Text(
+                          'Ich stimme der Datenschutzerklärung zu',
+                          style: TextStyle(color: Colors.black, fontSize: 12.0),
+                        ),
+                        value: false, 
+                        onChanged: (bool? value) {
+                          // Aktion implementieren
+                        },
+                        controlAffinity: ListTileControlAffinity.leading,
+                        contentPadding: EdgeInsets.symmetric(horizontal: 0.0),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
-          ),
+          ],
         ),
       ),
     );
